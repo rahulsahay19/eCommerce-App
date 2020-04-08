2346ffccbd1287a010dae44bd18d1ba52093a3d7
Rahul Sahay <rahulsahay19@hotmail.com>
2020-04-08 12:25:55 +0530

    added brands specification

--- Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq.Expressions;
 using Core.Entities;
 
 namespace Core.Specifications
@@ -5,6 +7,12 @@ namespace Core.Specifications
     public class ProductWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
         public ProductWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
